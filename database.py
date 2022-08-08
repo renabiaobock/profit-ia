0acaeac41391494d32104d3928157f8cbc00282e
renabiaobock <rena.biaobock.costa@gmail.com>
2022-08-08 18:07:22 -0300

    created database file and database conection and cursor

---
+++ database.py
@@ -0,0 +1,14 @@
+import psycopg2
+import CONSTANTS
+
+# database connection
+con = psycopg2.connect(host=CONSTANTS.HOST,
+                       database=CONSTANTS.DATABASE,
+                       user=CONSTANTS.USER,
+                       password=CONSTANTS.PASSWORD)
+
+#database cursor
+cur= con.cursor()
+
+
+
