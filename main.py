49b78af8a79a37d719587750bcb4621c364a5b4b
renabiaobock <rena.biaobock.costa@gmail.com>
2022-08-08 19:32:40 -0300

    added username and password

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from iqoptionapi.stable_api import IQ_Option
 import logging
+import CONSTANTS
 
 
 logging.disable(level=(logging.DEBUG))
 
-IQ = IQ_Option('aquila.mitra@gmail.com', 'Renancosta2@')
+IQ = IQ_Option(CONSTANTS.IQUSER, CONSTANTS.IQPASSWORD)
 check, reason = IQ.connect()
 
