0051a3d13f0b7882c3032ab3bb64017ece9ed2e0
renabiaobock <rena.biaobock.costa@gmail.com>
2022-08-09 11:07:25 -0300

    added change trade type functionality

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ logging.disable(level=(logging.DEBUG))
 "Conect to IQ account"
 trader.connect_to_iq()
 
+"Change trade type"
+trader.change_trade_type('PRACTICE')
+
--- trader.py
+++ trader.py
@@ -12,3 +12,8 @@ IQ = IQ_Option(CONSTANTS.IQUSER, CONSTANTS.IQPASSWORD)
 def connect_to_iq():
     IQ.connect()
 
+
+def change_trade_type(trade_type):
+    "PRACTICE/REAL"
+    IQ.change_balance(trade_type)
+
