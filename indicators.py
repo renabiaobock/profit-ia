ac78cf32511a0279b4067452b73089efd8b3c31f
renabiaobock <rena.biaobock.costa@gmail.com>
2022-08-10 17:55:44 -0300

    added functionality to generate candle data from a candle stream, added functionality to generate EMA, added functionality to generate EMA from asset

--- indicators.py
+++ indicators.py
@@ -2,3 +2,38 @@
 Module that calculate indicators from candle data
 """
 
+import time
+import numpy as np
+from talib.abstract import (EMA)
+
+import trader
+
+
+def generate_candle_data_from_candle_stream(asset, timeframe_in_minutes):
+    candles=trader.get_real_time_candles(asset, timeframe_in_minutes)
+
+    candle_data = {
+        'open': np.array([]),
+        'high': np.array([]),
+        'low': np.array([]),
+        'close': np.array([]),
+        'volume': np.array([])
+    }
+
+    for timestamp in candles:
+        candle_data["open"] = np.append(candle_data["open"], candles[timestamp]["open"])
+        candle_data["high"] = np.append(candle_data["high"], candles[timestamp]["max"])
+        candle_data["low"] = np.append(candle_data["low"], candles[timestamp]["min"])
+        candle_data["close"] = np.append(candle_data["close"], candles[timestamp]["close"])
+        candle_data["volume"] = np.append(candle_data["volume"], candles[timestamp]["volume"])
+    return candle_data
+
+
+def calculate_EMA(candle_data, EMA_time_period):
+    return EMA(candle_data, timeperiod=EMA_time_period)
+
+
+def calculate_asset_EMA(asset, timeframe_in_minutes, EMA_time_period):
+    candle_data = generate_candle_data_from_candle_stream(asset, timeframe_in_minutes)
+    return calculate_EMA(candle_data, EMA_time_period)
+
