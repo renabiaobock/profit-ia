e529bd234681acd57c69c64156d3502f611a39b4
renabiaobock <rena.biaobock.costa@gmail.com>
2022-08-11 19:19:26 -0300

    added last candle disruption check to bollinger bands strategy

--- strategy.py
+++ strategy.py
@@ -15,10 +15,18 @@ def check_entry_BOLLINGER_BANDS_EMA(asset, timeframe_in_minutes,
     bollinger_bands_last_lower = bollinger_bands['lower'][-1]
     #EMA
     ema = indicators.calculate_asset_EMA(asset, timeframe_in_minutes, EMA_time_period)[-1]
+    #Last candle disruption
+    last_candle_close = indicators.generate_candle_data_from_candle_stream(asset, timeframe_in_minutes)['close'][-2]
+    last_candle_bollinger_bands_upper = bollinger_bands['upper'][-2]
+    last_candle_bollinger_bands_lower = bollinger_bands['lower'][-2]
     # Strategy logic
-    if (price > bollinger_bands_last_upper) and (ema > bollinger_bands_last_upper):
+    if (price > bollinger_bands_last_upper) and\
+       (ema > bollinger_bands_last_upper) and\
+       (last_candle_close < last_candle_bollinger_bands_upper):
         return 'put'
-    elif (price < bollinger_bands_last_lower) and (ema < bollinger_bands_last_lower):
+    elif (price < bollinger_bands_last_lower) and\
+         (ema < bollinger_bands_last_lower) and\
+         (last_candle_close > last_candle_bollinger_bands_lower):
         return 'call'
     else:
         return None
